1b2deb49b2eac100a73a30045d5124a5b919ae37
Peter Vietense <peter.vietense@hotmail.de>
2020-08-10 21:38:27 +0200

    fixed AssemblyInfos

--- src/Dawn/Dawn.Wpf/Properties/AssemblyInfo.cs
+++ src/Dawn/Dawn.Wpf/Properties/AssemblyInfo.cs
@@ -1,5 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows;
 
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("Dawn")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b75d7d5-005a-44c1-a9f4-73b520517189")]
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
--- src/Dawn/Dawn.Wpf/Properties/SharedAssemblyInfo.cs
+++ src/Dawn/Dawn.Wpf/Properties/SharedAssemblyInfo.cs
@@ -2,6 +2,15 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("SoftThorn")]
 [assembly: AssemblyProduct("Dawn")]
-[assembly: AssemblyTitle("Dawn")]
 [assembly: AssemblyCopyright("© 2020 Insire")]
 [assembly: AssemblyTrademark("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
