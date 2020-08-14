a41a5349a87f46fad41f75cf1e500db4efd4d33a
Peter Vietense <peter.vietense@infact-wws.de>
2020-08-14 18:28:41 +0200

    dont ignore backup files

---
+++ src/Dawn/Dawn.Wpf/Features/Backups/BackupModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dawn.Wpf
+{
+    public sealed class BackupModel
+    {
+        public string FullPath { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ src/Dawn/Dawn.Wpf/Features/Backups/BackupViewModelFactory.cs
@@ -0,0 +1,27 @@
+﻿using MvvmScarletToolkit;
+using Serilog;
+using System;
+
+namespace Dawn.Wpf
+{
+    public sealed class BackupViewModelFactory
+    {
+        private readonly ConfigurationViewModel _configurationViewModel;
+        private readonly LogViewModel _logViewModel;
+        private readonly ILogger _log;
+        private readonly IScarletCommandBuilder _commandBuilder;
+
+        public BackupViewModelFactory(ConfigurationViewModel configurationViewModel, LogViewModel logViewModel, ILogger log, IScarletCommandBuilder commandBuilder)
+        {
+            _configurationViewModel = configurationViewModel ?? throw new ArgumentNullException(nameof(configurationViewModel));
+            _logViewModel = logViewModel ?? throw new ArgumentNullException(nameof(logViewModel));
+            _log = log ?? throw new ArgumentNullException(nameof(log));
+            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
+        }
+
+        public BackupViewModel Get(BackupModel model, BackupsViewModel backupsViewModel, Func<bool> onDeleteRequested, Action onDeleting)
+        {
+            return new BackupViewModel(_commandBuilder, model, backupsViewModel, _logViewModel, _log, _configurationViewModel, onDeleteRequested, onDeleting);
+        }
+    }
+}
