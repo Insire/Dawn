88d10935bc72b0526a9b281a7bf57e60e0493f95
Peter Vietense <peter.vietense@hotmail.de>
2025-04-26 15:57:30 +0200

    finalize implementation for avalonia ChangeDetectionWindow

---
+++ src/Dawn/Dawn.Avalonia/ChangeDetectionStateEnumEqualityConverter.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Dawn.Core.Features.ChangeDetection;
+using System;
+using System.Globalization;
+
+namespace Dawn.Avalonia
+{
+    public sealed class ChangeDetectionStateEnumEqualityConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not ChangeDetectionState state)
+            {
+                return false;
+            }
+
+            if (parameter is ChangeDetectionState param)
+            {
+                return state == param;
+            }
+
+            return false;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            // If we want to convert back, we need to subtract instead of add.
+            return BindingOperations.DoNothing;
+        }
+    }
+}
---
+++ src/Dawn/Dawn.Avalonia/LongToFileSizeConverter.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Dawn.Core.Features.ChangeDetection;
+using Dawn.Core.Features.Util;
+using System;
+using System.Globalization;
+
+namespace Dawn.Avalonia
+{
+    public sealed class LongToFileSizeConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is long number)
+            {
+                return number.GetBytesReadable();
+            }
+
+            return BindingOperations.DoNothing;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            // If we want to convert back, we need to subtract instead of add.
+            return BindingOperations.DoNothing;
+        }
+    }
+}
